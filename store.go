08620e55da7a0c1d78015aa5f71e07543ff71a9b
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-02-26 08:04:29 +0900

    Store

--- store.go
+++ store.go
@@ -1,1 +1,27 @@
 package gstore
+
+var s = make(map[string][]string)
+
+//Store :
+type Store []string
+
+//Open : open store object
+func Open(id string) Store {
+	s[id] = nil
+	return s[id]
+}
+
+//Get Values
+func (x Store) Get() []string {
+	return x
+}
+
+//Append Value
+func (x Store) Append(v ...string) {
+	x = append(x, v...)
+}
+
+//Clear Values
+func (x Store) Clear() {
+	x = nil
+}
